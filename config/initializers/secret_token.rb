dfa5c5a44c83f881d48d6d562ab3fbcf15c9f57a
Austin Freel <austin.freel@gmail.com>
2013-01-14 23:37:06 -0500

    first new commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlareHubApp::Application.config.secret_token = '8472cd5210ab9ae1598bf0189d6aae13b4808f7902bc6325b51bffe6d331d14e695ffeb757f0d234d45d187b93af83b91abb91abbf1d838463dd4e3f111e1b66'
